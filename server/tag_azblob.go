f27417cb7f47ca593ae018718b49a30b776780cb
mojatter <uz@trycatch.jp>
2026-04-18 12:20:36 +0900

    feat(server): Add build tags to enable cloud backends (#62)
    Add tag_s3.go, tag_gcs.go, tag_azblob.go guarded by server_s3,
    server_gcs, server_azblob build tags respectively. The default build
    remains osfs/memfs only.

---
+++ server/tag_azblob.go
@@ -0,0 +1,5 @@
+//go:build server_azblob
+
+package server
+
+import _ "github.com/mojatter/s2/azblob" // Register Azure Blob Storage backend for s2-server.
---
+++ server/tag_s3.go
@@ -0,0 +1,5 @@
+//go:build server_s3
+
+package server
+
+import _ "github.com/mojatter/s2/s3" // Register S3 backend for s2-server.
